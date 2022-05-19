747a793c91df17d23728e1ee32f34a1bcf99cc54
almallar <99345851+almallar@users.noreply.github.com>
2022-05-19 10:43:05 +0200

    Update unit_test.sh

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -8,5 +8,7 @@ export DYNAMODB_TABLE=todoUnitTestsTable
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
-coverage report
+#Modificacion alex
+#coverage report 
+#coverage report -m
 coverage xml
