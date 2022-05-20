06838245a4289be542416274187c38b0fc3c1cb3
almallar <99345851+almallar@users.noreply.github.com>
2022-05-20 13:27:46 +0200

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -75,6 +75,20 @@ class TestDatabaseFunctions(unittest.TestCase):
         #                 'ResponseMetadata']['HTTPStatusCode'])
         print ('End: test_put_todo')
 
+        def test_put_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_put_todo_error')
+        # Testing file functions
+        from src.todoList import put_item
+        # Table local
+        response = put_item(self.text, self.dynamodb)
+        print ('Response put_item:' + str(response))
+        self.assertEqual(800, response['statusCode'])
+        # Table mock
+        #self.assertEqual(200, put_item(self.text, self.dynamodb)[
+        #                 'ResponseMetadata']['HTTPStatusCode'])
+        print ('End: test_put_todo')
+        
     def test_get_todo(self):
         print ('---------------------')
         print ('Start: test_get_todo')
