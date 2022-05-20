782cdedf7fe983e0ae85bb8207e124f5eee485ea
almallar <99345851+almallar@users.noreply.github.com>
2022-05-20 15:28:06 +0200

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -38,23 +38,6 @@ def get_item(key, dynamodb=None):
             return result['Item']
 
 
-def get_item_prueba(key, dynamodb=None):
-    table = get_table(dynamodb)
-    try:
-        result = table.get_item(
-            Key={
-                'idd': key
-            }
-        )
-
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    else:
-        print('Result getItem:'+str(result))
-        if 'Item' in result:
-            return result['Item']
-
-
 def get_items(dynamodb=None):
     table = get_table(dynamodb)
     # fetch todo from the database
