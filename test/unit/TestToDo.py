c02ed3eac5f5a2d2158824e83acdc11c7d3109f1
almallar <99345851+almallar@users.noreply.github.com>
2022-05-20 08:50:47 +0200

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -105,6 +105,15 @@ class TestDatabaseFunctions(unittest.TestCase):
             responseGet['text'])
         print ('End: test_get_todo')
     
+    def test_get_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_get_todo_error')
+        from src.todoList import get_item
+        from src.todoList import put_item
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        print ('Start: test_get_todo_error')
+    
     def test_list_todo(self):
         print ('---------------------')
         print ('Start: test_list_todo')
