f4b2a277e896b49fe527b1a1d655c885336193f9
almallar <99345851+almallar@users.noreply.github.com>
2022-05-21 08:58:50 +0200

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -104,11 +104,13 @@ def delete_item(key, dynamodb=None):
     table = get_table(dynamodb)
     # delete the todo from the database
     try:
-        table.delete_item(
-            Key={
-                'id': key
-            }
-        )
+        if table.delete_item(
+                Key={
+                    'id': key
+                }
+            )
+        else 
+            raise ClientError("El valor no es un numero")
 
     except ClientError as e:
         print(e.response['Error']['Message'])
