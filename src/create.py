97101a680cab9086b03729fcdfbecc3567932b81
almallar <99345851+almallar@users.noreply.github.com>
2022-05-21 20:43:55 +0200

    Update create.py

--- src/create.py
+++ src/create.py
@@ -5,13 +5,16 @@ import todoList
 
 def create(event, context):
     data = json.loads(event['body'])
-    if 'text' not in data:
-        logging.error("Validation failed")
-        raise Exception("Couldn't create the todo item.")
-    item = todoList.put_item(data['text'])
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(item)
-    }
-    return response
+    try:
+        if 'text' not in data:
+            logging.error("Validation failed")
+            raise Exception("Couldn't create the todo item.")
+        item = todoList.put_item(data['text'])
+        # create a response
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(item)
+        }
+        return response
+    except:
+        print("es un error")
