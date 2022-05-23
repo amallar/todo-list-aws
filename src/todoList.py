891df164245bee2bf54962899aa20971b5d7fd93
almallar <99345851+almallar@users.noreply.github.com>
2022-05-23 09:46:59 +0200

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -48,6 +48,7 @@ def put_item(text, dynamodb=None):
     table = get_table(dynamodb)
     timestamp = str(time.time())
     print('Table name:' + table.name)
+    print('Aca Imprime el texto:' + text)
     item = {
         'id': str(uuid.uuid1()),
         'text': text,
@@ -55,7 +56,7 @@ def put_item(text, dynamodb=None):
         'createdAt': timestamp,
         'updatedAt': timestamp,
     }
-    if text !="":
+    if text != '':
         # write the todo to the database
         table.put_item(Item=item)
         # create a response
@@ -65,7 +66,7 @@ def put_item(text, dynamodb=None):
         }
     else:
         print('Se produjo un error')
-        response='Se ha producido un error'
+        response = 'Se ha producido un error'
     return response
 
 
